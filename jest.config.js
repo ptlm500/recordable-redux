e47dbe71f6c6989b3a081629fdf86294c15c4cf6
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-22 17:58:21 +0000

    Add coverage to jest configuration

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    '\\\\node_modules\\\\'
+  ],
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: [
+    '\\\\node_modules\\\\'
+  ],
+
+};
